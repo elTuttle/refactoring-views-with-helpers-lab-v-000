9820d123b9ea6d9a4a87f435ca186e6c673d4e13
John Tuttle <thejwt@gmail.com>
2017-11-26 22:59:32 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
 
   def display_artist(song)
-    if song.artist_name != nil
+    if song.artist != nil
       link_to song.artist_name, artist_path(song.artist)
     else
       link_to 'Add Artist', song_path(song)
